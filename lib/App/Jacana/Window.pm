715b09fe9bdc6a3aec0bd008cb6dfbf3a642e554
Ben Morrow <ben@morrow.me.uk>
2014-01-14 03:43:57 +0000

    Build the toolbar with UIManager.
    This means I get a menu and keybindings for relatively little work.

--- lib/App/Jacana/Window.pm
+++ lib/App/Jacana/Window.pm
@@ -7,12 +7,16 @@ use warnings;
 use Moo;
 use MooX::MethodAttributes;
 
+use YAML::XS ();
+
 with qw/ App::Jacana::HasApp MooX::Gtk2 /;
 
 has frame       => is => "lazy";
 has status_bar  => is => "lazy";
-has toolbar     => is => "lazy";
 has view        => is => "ro";
+
+has actions     => is => "lazy";
+has uimgr       => is => "lazy";
 
 # frame
 
@@ -24,8 +28,18 @@ sub _build_frame {
     $w->set_title("Jacana");
     $w->set_default_size(400, 300);
 
+    my $ui = $self->uimgr;
+    $ui->ensure_update;
+    $w->add_accel_group($ui->get_accel_group);
+
     my $vb = Gtk2::VBox->new;
-    $vb->pack_start($self->toolbar, 0, 0, 0);
+    $vb->pack_start($_, 0, 0, 0) for $ui->get_toplevels("menubar");
+    for ($ui->get_toplevels("toolbar")) {
+        $_->set_style("icons");
+        $_->set_icon_size("small-toolbar");
+        $vb->pack_start($_, 0, 0, 0);
+    }
+
     $vb->pack_start($self->view->widget, 1, 1, 0);
     $vb->pack_start($self->status_bar, 0, 0, 0);
 
@@ -33,27 +47,63 @@ sub _build_frame {
     $w;
 }
 
-sub _destroy_frame :Signal(frame::destroy) { 
+sub _quit : Signal(frame::destroy) Action(Quit) { 
     Gtk2->main_quit;
 }
 
-# toolbar
+# actions
 
-sub _build_toolbar {
+sub _build_actions {
     my ($self) = @_;
-    my $bar = Gtk2::Toolbar->new;
-    my $but = Gtk2::ToolButton->new_from_stock("gtk-media-play");
-    $but->signal_connect("clicked", sub { $self->_play_music });
-    $bar->insert($but, -1);
-    $bar;
+    my $actions = YAML::XS::Load <<YAML;
+        FileMenu:
+            label:          File
+        Quit:
+            label:          Quit
+            accelerator:    "<Ctrl>Q"
+
+        MidiMenu:
+            label:          MIDI
+        MidiPlay:
+            stock_id:       gtk-media-play
+            label:          Play
+            accelerator:    F5
+YAML
+    my $grp = Gtk2::ActionGroup->new("edit");
+    $grp->add_actions([
+        map +{ name => $_, %{$$actions{$_}} },
+        keys %$actions
+    ]);
+    $grp;
 }
 
-sub _play_music {
+sub _build_uimgr {
     my ($self) = @_;
+    my $ui = Gtk2::UIManager->new;
+    $ui->add_ui_from_string(<<XML);
+        <menubar>
+            <menu action="FileMenu">
+                <menuitem action="Quit"/>
+            </menu>
+            <menu action="MidiMenu">
+                <menuitem action="MidiPlay"/>
+            </menu>
+        </menubar>
+        <toolbar>
+            <toolitem action="MidiPlay"/>
+        </toolbar>
+XML
+    $ui->insert_action_group($self->actions, 0);
+    $ui;
+}
+
+sub _play_music :Action(MidiPlay) {
+    my ($self, $action) = @_;
     my $app     = $self->app;
     my $view    = $self->view;
 
     $self->set_status("playing");
+    $action->set_sensitive(0);
 
     $app->midi->play_music(
         $app->document->music,
@@ -61,6 +111,7 @@ sub _play_music {
         sub { $view->playing_off($_[0]) },
         sub { 
             $self->set_status("");
+            $action->set_sensitive(1);
         },
     );
 }
