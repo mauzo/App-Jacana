ac0d1b249123c4bd1b3e5e13e9c6fcc5a1bc30ff
Ben Morrow <ben@morrow.me.uk>
2017-11-03 10:25:32 +0000

    Let's have some more types here.

--- lib/App/Jacana/Has/Time.pm
+++ lib/App/Jacana/Has/Time.pm
@@ -1,25 +1,24 @@
 package App::Jacana::Has::Time;
 
-use Moose::Role;
-use MooseX::AttributeShortcuts;
+use App::Jacana::Moose -role;
 use MooseX::Copiable;
 
 use App::Jacana::Util::Length;
 
 has beats => (
     is      => "rw",
-    traits      => [qw/Copiable/],
-    #isa     => Int,
+    traits  => [qw/Copiable/],
+    isa     => Int,
 );
 has divisor => (
     is      => "rw",
-    traits      => [qw/Copiable/],
-    #isa     => Enum[qw/1 2 4 8 16 32/],
+    traits  => [qw/Copiable/],
+    isa     => Enum[qw/1 2 4 8 16 32/],
 );
 has partial => (
     is          => "rw",
     traits      => [qw/Copiable/],
-    #isa         => Maybe[ConsumerOf["App::Jacana::Has::Length"]],
+    isa         => Has "Length",
     deep_copy   => 1,
     predicate   => 1,
     clearer     => 1,
