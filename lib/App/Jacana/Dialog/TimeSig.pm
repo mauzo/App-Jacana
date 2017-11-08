f85c09b7f7ae11b2b3c87e6de107184655169f12
Ben Morrow <ben@morrow.me.uk>
2017-11-08 09:49:14 +0000

    Move the TimeSig partial into a separate class.
    I want a dialog widget for 'a note length', since tempo needs it too.

--- lib/App/Jacana/Dialog/TimeSig.pm
+++ lib/App/Jacana/Dialog/TimeSig.pm
@@ -4,6 +4,7 @@ use 5.012;
 use App::Jacana::Moose;
 use MooseX::Gtk2;
 
+use App::Jacana::Dialog::Widget::Length;
 use App::Jacana::Gtk2::ComboBox;
 
 use Data::Dump      qw/pp/;
@@ -16,30 +17,9 @@ with    qw/
     App::Jacana::Has::Time
 /;
 
-package App::Jacana::Dialog::TimeSig::Partial {
-    use App::Jacana::Moose;
-    use MooseX::Gtk2;
-    use namespace::autoclean;
-
-    with qw/ App::Jacana::Has::Length/;
-
-    has dialog      => is => "ro", weak_ref => 1;
-    has "+length"   => (
-        traits      => ["Gtk2"],
-        gtk_prop    => "dialog._plen.current-value",
-    );
-    has "+dots"     => (
-        traits      => ["Gtk2"],
-        gtk_prop    => "dialog._pdots.current-value",
-    );
-
-    1;
-}
-
 has _beats      => is => "lazy";
 has _divisor    => is => "lazy";
-has _plen       => is => "lazy";
-has _pdots      => is => "lazy";
+has _partial    => is => "lazy";
 has _p_check    => is => "lazy";
 
 has "+beats"    => (
@@ -56,44 +36,37 @@ has "+divisor"  => (
     default     => 4,
     gtk_prop    => "_divisor.current-value"
 );
-has "+partial"  => (
-    traits      => ["Coercer"],
-);
+has "+partial"  => isa => My "Dialog::Widget::Length", coerce => 1;
 
 sub title   { "Time signature" }
 
 sub BUILD {
     my ($self) = @_;
+
     if ($self->has_partial) {
         $self->_p_check->set_active(1);
-    }
-    else {
-        $self->$_->set_sensitive(0) for qw/_plen _pdots/;
+        # Hmm hacky hacky hack
+        # I think the signal isn't installed early enough
+        $self->_notify_p_check;
     }
 }
 
 sub _notify_p_check :Signal(_p_check.notify::active) {
     my ($self) = @_;
-    my $active = $self->_p_check->get_active;
+    my $active  = $self->_p_check->get_active;
+    my $part    = $self->_partial;
 
     if ($active) {
         $self->has_partial
             or $self->partial({length => 4, dots => 0});
+        my $wid = $self->partial->widget;
+        $part->add($wid);
+        $part->show_all;
     }
     else {
+        $part->remove($part->get_child);
         $self->clear_partial;
     }
-    $self->$_->set_sensitive($active) for qw/_plen _pdots/;
-}
-
-sub _coerce_partial {
-    my ($self, $new) = @_;
-
-    warn "Dialog::TimeSig: COERCE FROM: " . pp $new;
-    App::Jacana::Dialog::TimeSig::Partial->new(
-        %$new,
-        dialog => $self,
-    );
 }
 
 sub _build__beats { Gtk2::Entry->new }
@@ -104,23 +77,16 @@ sub _build__divisor {
     $cb;
 }
 
-sub _build__plen {
-    my $cb = App::Jacana::Gtk2::ComboBox->new;
-    $cb->add_pairs(qw/
-        1 semibreve 2 minim 3 crotchet 4 quaver
-        5 semiquaver 6 d.s.quaver 7 h.d.s.quaver
-    /);
-    $cb;
-}
-
-sub _build__pdots {
-    my $cb = App::Jacana::Gtk2::ComboBox->new;
-    $cb->add_pairs(qw/0 0 1 1 2 2/);
-    $cb;
-}
-
 sub _build__p_check {
     Gtk2::CheckButton->new_with_label("Upbeat:");
+}
+
+sub _build__partial {
+    my ($self) = @_;
+
+    my $f   = Gtk2::Frame->new;
+    $f->set_label_widget($self->_p_check);
+    $f;
 }
 
 sub _build_content_area {
@@ -135,12 +101,7 @@ sub _build_content_area {
     $hb->pack_start($ts, 1, 0, 0);
 
     $vb->pack_start($hb, 1, 0, 0);
-
-    my $par = Gtk2::HBox->new;
-    $par->pack_start($self->_p_check, 1, 0, 0);
-    $par->pack_start($self->_plen, 1, 0, 0);
-    $par->pack_start($self->_pdots, 1, 0, 0);
-    $vb->pack_start($par, 1, 0, 0);
+    $vb->pack_start($self->_partial, 1, 0, 0);
 }
 
 1;
---
+++ lib/App/Jacana/Dialog/Widget/Length.pm
@@ -0,0 +1,54 @@
+package App::Jacana::Dialog::Widget::Length;
+
+use App::Jacana::Moose;
+use MooseX::Gtk2;
+
+use App::Jacana::Gtk2::ComboBox;
+
+use namespace::autoclean;
+
+with qw/ App::Jacana::Has::Length/;
+
+has dialog      => is => "ro", weak_ref => 1;
+has "+length"   => (
+    traits      => ["Gtk2"],
+    gtk_prop    => "_length.current-value",
+);
+has "+dots"     => (
+    traits      => ["Gtk2"],
+    gtk_prop    => "_dots.current-value",
+);
+
+has _length => is => "lazy";
+has _dots   => is => "lazy";
+
+has widget  => is => "lazy";
+
+sub _build__length {
+    my $cb = App::Jacana::Gtk2::ComboBox->new;
+    $cb->add_pairs(qw/
+        1 semibreve 2 minim 3 crotchet 4 quaver
+        5 semiquaver 6 d.s.quaver 7 h.d.s.quaver
+    /);
+    $cb;
+}
+
+sub _build__dots {
+    my $cb = App::Jacana::Gtk2::ComboBox->new;
+    $cb->add_pairs(qw/0 0 1 1 2 2/);
+    $cb;
+}
+
+sub _build_widget {
+    my ($self) = @_;
+
+    my $hbox = Gtk2::HBox->new;
+    $hbox->pack_start($self->_length, 1, 0, 0);
+    $hbox->pack_start($self->_dots, 1, 0, 0);
+
+    warn "BUILD WIDGET [$self] [$hbox]";
+
+    $hbox;
+}
+
+1;
